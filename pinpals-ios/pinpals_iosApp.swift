78f609b90f4138ffc09e7dcec30924fb5e9e1c71
Noah Bailey <bailey5407@gmail.com>
2024-06-15 17:04:25 -0600

    Initial Commit

---
+++ pinpals-ios/pinpals_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinpals_iosApp.swift
+//  pinpals-ios
+//
+//  Created by Noah Bailey on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct pinpals_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
